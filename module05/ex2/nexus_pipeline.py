93d5705cd7ee344e3986a58857d2c7879df3fd4f
bmarbouh <strtem73@gmail.com>
2026-02-21 17:29:25 +0000

    ex2

--- module05/ex2/nexus_pipeline.py
+++ module05/ex2/nexus_pipeline.py
@@ -1,10 +1,73 @@
 #!/usr/bin/env python3
+from typing import Protocol, Any, Union, Optional
+from abc import ABC, abstractmethod
 
 
+class ProcessingStage(Protocol):
+    def process(self, data: Any) -> Any:
+        pass
 
 
+class InputStage:
+    def process(self, data: Any) -> Any:
+        pass
 
 
+class TransformStage:
+    def process(self, data: Any) -> Any:
+        pass
+
+
+class OutputStage:
+    def process(self, data: Any) -> Any:
+        pass
+
+
+class ProcessingPipeline(ABC):
+    def __init__(self, pipeline_id):
+        self.pipeline_id = pipeline_id
+        self.stages = []
+    
+    def add_stage(self,stage :ProcessingStage):
+        pass
+
+    @abstractmethod
+    def process(self, data: Any) -> Any:
+        pass
+
+
+class JSONAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id):
+        super().__init__(pipeline_id)
+    
+    def process(self, data: Any) -> Any:
+        pass
+
+class CSVAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id):
+        super().__init__(pipeline_id)
+    
+    def process(self, data: Any) -> Any:
+        pass
+
+
+class StreamAdapter(ProcessingPipeline):
+    def __init__(self, pipeline_id):
+        super().__init__(pipeline_id)
+    
+    def process(self, data: Any) -> Any:
+        pass
+
+
+class NexusManager:
+    def __init__(self):
+        self.piplens = []
+
+    def add_pipline(self, pipline: ProcessingPipeline):
+        pass
+
+    def process_data(self, Data: Any):
+        pass
 
 
 def main() -> None:
