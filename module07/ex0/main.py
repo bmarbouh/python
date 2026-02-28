d924d9388888d3e19407b545ec96be9d7d5da8d9
bmarbouh <strtem73@gmail.com>
2026-02-28 04:03:10 +0000

    module07

---
+++ module07/ex0/main.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Card(ABC):
+    def __init__(self, name: str, cost: int, rarity: str) -> None:
+        self.name = name
+        self.cost = cost
+        self.rarity = rarity
+    
+    @abstractmethod
+    def play(self, game_state: dict):
+        pass
+    
+    def  get_card_info(self):
+        pass
+    
+    def is_playable(self, available_mana: int):
+        pass
