f5ff83f7b6cce864bd326c17439c6c484dc3b57d
bmarbouh <strtem73@gmail.com>
2026-03-02 20:49:19 +0000

    ex3 en progress

---
+++ module07/ex3/FantasyCardFactory.py
@@ -0,0 +1,3 @@
+from ex0.CreatureCard import CreatureCard
+from ex1.ArtifactCard import ArtifactCard
+from ex1.SpellCard import SpellCard
---
+++ module07/ex3/main.py
@@ -0,0 +1,9 @@
+
+
+
+def main():
+    print("\n=== DataDeck Game Engine ===\n")
+
+
+if __name__ == "__main__":
+    main()
